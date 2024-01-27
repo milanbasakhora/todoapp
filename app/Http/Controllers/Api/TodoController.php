dd5a88141139dccc87c08d612272526c60298106
KirawaYo365 <kirawa.ya.files@gmail.com>
2024-01-27 11:43:10 +0545

    todo app api

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -12,7 +14,16 @@ class TodoController extends Controller
      */
     public function index()
     {
-        print("hello");
+        $todos = Todo::all();
+        if ($todos) {
+            return response()->json($todos, 200);
+        } else {
+            $data = [
+                'success' => true,
+                'message' => 'There is no data.'
+            ];
+            return response()->json($data, 200);
+        }
     }
 
     /**
@@ -20,7 +31,27 @@ class TodoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'task' => 'required',
+            'status' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                "success" => false,
+                "message" => $validator->errors(),
+            ];
+            return response()->json($data, 400);
+        }
+
+        Todo::create($request->all());
+
+        $data = [
+            "success" => true,
+            "message" => "Success",
+        ];
+
+        return response()->json($data, 200);
     }
 
     /**
@@ -28,7 +59,13 @@ class TodoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $todo = Todo::find($id);
+
+        if (!$todo) {
+            return response()->json(['message' => 'List Not Found!'], 404);
+        }
+
+        return response()->json($todo, 200);
     }
 
     /**
@@ -36,7 +73,34 @@ class TodoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $todo = Todo::find($id);
+        // return $request->all();xxx
+        if (!$todo) {
+            return response()->json(['message' => 'Not Found!', 404]);
+        }
+
+        // Validation
+        $validator = Validator::make($request->all(), [
+            'task' => 'required',
+            'status' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                "success" => false,
+                "message" => $validator->errors(),
+            ];
+            return response()->json($data, 400);
+        }
+
+        $todo->update($request->all());
+
+        $data = [
+            "success" => true,
+            "message" => "Update Success",
+        ];
+
+        return response()->json($data, 200);
     }
 
     /**
@@ -44,6 +108,16 @@ class TodoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $todo = Todo::find($id);
+
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found!'], 404);
+        }
+
+        $todo->delete();
+
+        return response()->json([
+            'message' => 'Delete success'
+        ], 200);
     }
 }
